b4e5c8c6e121361b90cdf3734ff19e7ed8ae1f3e
RogerIn1900 <150680014+RogerIn1900@users.noreply.github.com>
2025-11-24 18:32:55 +0800

    test2-登录检查逻辑

--- app/src/main/java/com/example/test2/LoginActivity.java
+++ app/src/main/java/com/example/test2/LoginActivity.java
@@ -1,25 +1,96 @@
 package com.example.test2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText etInputEmail,etInputPassword;
+    LinearLayout btWechat,btApple;
+    SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_login);
 
-        MaterialButton LoginBotton = findViewById(R.id.btLogin);
-        LoginBotton.setOnClickListener(v -> {
+        sp = getApplicationContext().getSharedPreferences("UserInfoSP", Context.MODE_PRIVATE);
 
-            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+        etInputEmail = findViewById(R.id.etInputEmail);
+        etInputPassword = findViewById(R.id.etInputPassword);
+        btWechat = findViewById(R.id.btWechat);
+        btApple = findViewById(R.id.btApple);
+        MaterialButton LoginBotton = findViewById(R.id.btLogin);
+
+        saveDefaultUserInfo();
+
+
+        etInputPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                if (s.length() < 6) {
+                    etInputPassword.setError("密码⻓度不能少于6位");
+                } else {
+                    etInputPassword.setError(null); // 清除错误提⽰
+                }
+                Toast.makeText(LoginActivity.this, "密码长度正确", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
         });
+
+        LoginBotton.setOnClickListener(v -> {
+            String inputEmail = etInputEmail.getText().toString();
+            String inputPassword = etInputPassword.getText().toString();
+
+            String email = sp.getString("email", "");
+            String password = sp.getString("password", "");
+
+            if (inputEmail.equals(email) && inputPassword.equals(password)) {
+                Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+
+            } else {
+                Toast.makeText(LoginActivity.this, "登录失败,请检查邮箱 密码是否正确", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+
+        btWechat.setOnClickListener(v -> {
+            Toast.makeText(LoginActivity.this, "微信登录", Toast.LENGTH_SHORT).show();
+        });
+
+        btApple.setOnClickListener(v -> {
+            Toast.makeText(LoginActivity.this, "苹果登录", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void saveDefaultUserInfo() {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("email", "1750193717@qq.com");
+        editor.putString("password", "123456");
+        editor.apply();
+        boolean isSaveSuccess = editor.commit();
+        System.out.println("存储是否成功：" + isSaveSuccess); // true则存储成功
     }
 }
